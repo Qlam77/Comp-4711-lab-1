d28fabd8ceff04a3230cc4fbcdcdc7ddcc5a6893
qlam77 <qlam3@my.bcit.ca>
2018-01-10 12:05:16 -0800

    Added Comments to class and index

--- Student.php
+++ Student.php
@@ -1,31 +1,30 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * Student Class
  *
- * @author quinc
+ * @author Quincy
  */
 class Student {
+    //constructor for student class
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
+    
+    //add email method
     function add_email($which,$address) {
     $this->emails[$which] = $address;
 }
 
+    //add grade method
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //averages grades
     function average() {
     $total = 0;
     foreach ($this->grades as $value)
@@ -34,6 +33,7 @@ class Student {
     
     }
     
+    //prints message as a string
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
